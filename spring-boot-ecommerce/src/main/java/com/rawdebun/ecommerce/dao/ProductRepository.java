670ba12272cf01556cda1bbda184c90833c11a3a
fszamfi <zamfirflorinstelian@yahoo.com>
2021-03-19 22:47:44 +0200

    removed lombok dependency, updated repository interfaces

--- spring-boot-ecommerce/src/main/java/com/rawdebun/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/rawdebun/ecommerce/dao/ProductRepository.java
@@ -1,12 +1,22 @@
 package com.rawdebun.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rawdebun.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, 
+									Pageable pageable);
+
 }
+
+
+
--- spring-boot-ecommerce/src/main/java/com/rawdebun/ecommerce/entity/Product.java
+++ spring-boot-ecommerce/src/main/java/com/rawdebun/ecommerce/entity/Product.java
@@ -24,9 +24,6 @@ import lombok.Setter;
 
 @Entity
 @Table(name="product")
-//@Data
-@Getter
-@Setter
 @DynamicUpdate
 public class Product   {
 
@@ -180,6 +177,121 @@ public class Product   {
 				+ description + ", unitPrice=" + unitPrice + ", imageUrl=" + imageUrl + ", active=" + active
 				+ ", unitsInStock=" + unitsInStock + ", dateCreated=" + dateCreated + ", lastUpdated=" + lastUpdated
 				+ "]";
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public ProductCategory getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(ProductCategory category) {
+		this.category = category;
+	}
+
+
+	public String getSku() {
+		return sku;
+	}
+
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+
+
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+
+
+	public static Logger getLogger() {
+		return logger;
 	}
     
 }
